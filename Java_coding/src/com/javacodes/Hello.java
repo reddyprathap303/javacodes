3d0ebda5f0a384d267318e5ee10ae161c0545300
unknown <reddyprathap303@gmail.com>
2025-07-03 16:50:05 +0530

    second commit

---
+++ Java_coding/src/com/javacodes/Hello.java
@@ -0,0 +1,14 @@
+package com.javacodes;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String userName = sc.nextLine();
+		System.out.println("Hello " + userName);
+	}
+
+}
